6ef394631e31202b66bf37c99071f4e8c6e19778
Dejan Mircevski <github@mircevski.com>
2016-08-20 15:41:44 -0400

    Add oksyntax().

--- test/FooTest.cpp
+++ test/FooTest.cpp
@@ -1,9 +1,24 @@
+#include <string>
+
 #include "ramfuzz/lib/RamFuzz.hpp"
 #include "clang/Frontend/FrontendActions.h"
 #include "gtest/gtest.h"
 
 namespace {
 
-TEST(Foo, One) { EXPECT_NE("fail", ramfuzz("class X {};")); }
+using clang::SyntaxOnlyAction;
+using clang::tooling::runToolOnCode;
+using std::string;
+
+/// True if code has valid syntax.
+bool oksyntax(const string &code) {
+  return runToolOnCode(new SyntaxOnlyAction(), code);
+}
+
+TEST(OksyntaxTest, ValidCode) { EXPECT_TRUE(oksyntax("int a = 123;")); }
+TEST(OksyntaxTest, InvalidCode) { EXPECT_FALSE(oksyntax("int a =;")); }
+TEST(OksyntaxTest, FailLiteral) { EXPECT_FALSE(oksyntax("fail")); }
+
+TEST(RamFuzzTest, EmptyClass) { EXPECT_EQ("", ramfuzz("class X {};")); }
 
 } // anonymous namespace
