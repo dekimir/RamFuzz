d98d3f202609537b1adb81b7754eb41177232dc5
Dejan Mircevski <github@mircevski.com>
2016-08-19 22:32:22 -0400

    Make the unit test actually run RamFuzz. :)

--- test/FooTest.cpp
+++ test/FooTest.cpp
@@ -1,12 +1,22 @@
+#include <sstream>
+
+#include "ramfuzz/lib/RamFuzz.hpp"
 #include "clang/Frontend/FrontendActions.h"
 #include "clang/Tooling/Tooling.h"
 #include "gtest/gtest.h"
 
 namespace {
 
+using clang::ast_matchers::MatchFinder;
+using clang::tooling::newFrontendActionFactory;
+using std::ostringstream;
+
 TEST(Foo, One) {
-  EXPECT_TRUE(clang::tooling::runToolOnCode(new clang::SyntaxOnlyAction,
-                                            "class X {};"));
+  ostringstream str;
+  RamFuzz RF(str);
+  MatchFinder MF = RF.makeMatchFinder();
+  EXPECT_TRUE(clang::tooling::runToolOnCode(
+      newFrontendActionFactory(&MF)->create(), "class X {};"));
 }
 
 } // anonymous namespace
