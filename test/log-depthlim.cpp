5da12596d2f7dc03ef56c9547384162bb7cfd3ec
Dejan Mircevski <github@mircevski.com>
2016-10-02 16:55:06 -0400

    Avoid segfault in test/log-depthlim.cpp.

--- test/log-depthlim.cpp
+++ test/log-depthlim.cpp
@@ -2,6 +2,15 @@
 
 #include "fuzz.hpp"
 
+bool operator!=(const ramfuzz::qqA::control &a,
+                const ramfuzz::qqA::control &b) {
+  if (!a && !b)
+    return false;
+  if (bool(a) != bool(b))
+    return true;
+  return a.obj != b.obj;
+}
+
 int main() {
   using namespace ramfuzz::runtime;
   using namespace std;
@@ -9,7 +18,7 @@ int main() {
   auto rf1 = spin_roulette<ramfuzz::qqA::control>(*g);
   g.reset(new gen("fuzzlog1", "fuzzlog2"));
   auto rf2 = spin_roulette<ramfuzz::qqA::control>(*g);
-  return rf1.obj != rf2.obj;
+  return rf1 != rf2;
 }
 
 unsigned ::ramfuzz::runtime::spinlimit = 3;
