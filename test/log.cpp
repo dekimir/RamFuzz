479fd76605ce3cc569b1bce308e7baa1c5ca66df
Dejan Mircevski <github@mircevski.com>
2016-10-07 21:04:42 -0400

    Fix segfault in test/log.cpp.

--- test/log.cpp
+++ test/log.cpp
@@ -2,6 +2,15 @@
 
 #include "fuzz.hpp"
 
+bool operator!=(const ramfuzz::rfA::control &a,
+                const ramfuzz::rfA::control &b) {
+  if (!a && !b)
+    return false;
+  if (bool(a) != bool(b))
+    return true;
+  return a.obj != b.obj;
+}
+
 int main() {
   using namespace ramfuzz::runtime;
   using namespace std;
@@ -9,7 +18,7 @@ int main() {
   auto rf1 = spin_roulette<ramfuzz::rfA::control>(*g);
   g.reset(new gen("fuzzlog1", "fuzzlog2"));
   auto rf2 = spin_roulette<ramfuzz::rfA::control>(*g);
-  return rf1.obj != rf2.obj;
+  return rf1 != rf2;
 }
 
 unsigned ::ramfuzz::runtime::spinlimit = 3;
