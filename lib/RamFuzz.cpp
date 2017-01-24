cfc1b70647ad86d57899ebe9e5cf52b425376cb0
Dejan Mircevski <github@mircevski.com>
2017-01-24 11:59:18 -0500

    Make all gen::make() arguments int.
    I had thought they must be different to avoid the error "redefinition of
    make()", but it turns out enable_if is enough to keep them separate.
    
    Signed-off-by: Dejan Mircevski <github@mircevski.com>

--- lib/RamFuzz.cpp
+++ lib/RamFuzz.cpp
@@ -701,9 +701,9 @@ void RamFuzz::run(const MatchFinder::MatchResult &Result) {
 void RamFuzz::finish(const Inheritance &inh) {
   for (auto e : referenced_enums) {
     outh << "template<> " << e.first << "* ramfuzz::runtime::gen::make<"
-         << e.first << ">(float);\n";
+         << e.first << ">(int);\n";
     outc << "template<> " << e.first << "* ramfuzz::runtime::gen::make<"
-         << e.first << ">(float) {\n";
+         << e.first << ">(int) {\n";
     outc << "  static " << e.first << " a[] = {\n    ";
     int comma = 0;
     for (const auto &n : e.second)
