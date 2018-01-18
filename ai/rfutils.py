e348e89d663af16d487989bc21b2c0dcf4d4acf9
Dejan Mircevski <github@mircevski.com>
2018-01-17 19:38:04 -0500

    Update copyright notice to include 2018.
    Signed-off-by: Dejan Mircevski <github@mircevski.com>

--- ai/rfutils.py
+++ ai/rfutils.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright 2016-17 The RamFuzz contributors. All rights reserved.
+# Copyright 2016-2018 The RamFuzz contributors. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
