145eb670a4b8780cb94dbed892110422f131ccd3
Dejan Mircevski <github@mircevski.com>
2018-07-07 10:20:32 -0400

    Add TestEq.test_fork() to graph-test.py.
    Signed-off-by: Dejan Mircevski <github@mircevski.com>

--- ai/graph-test.py
+++ ai/graph-test.py
@@ -7,18 +7,23 @@ import unittest
 
 
 class TestEq(unittest.TestCase):
-    def check_lit(self, a, b):
+    def check_lit(self, a, b=None):
         """Asserts that nodes created from two literals are equal."""
+        if b is None:
+            b = a
         self.assertEqual(node.from_literal(a), node.from_literal(b))
 
     def test_empty(self):
-        self.check_lit([], [])
+        self.check_lit([])
 
     def test_seq1(self):
-        self.check_lit([(1.0, 1L)], [(1.0, 1L)])
+        self.check_lit([(1.0, 1L)])
 
     def test_seq2(self):
-        self.check_lit([(1.0, 1L), (2.0, 2L)], [(1.0, 1L), (2.0, 2L)])
+        self.check_lit([(1.0, 1L), (2.0, 2L)])
+
+    def test_fork(self):
+        self.check_lit([{1: [(1.0, 1L)], 2: [(2.0, 1L)]}])
 
 
 if __name__ == '__main__':
