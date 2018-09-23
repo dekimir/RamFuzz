753b31cd763d63f72b79be99be537cf2ae2dabd5
Dejan Mircevski <github@mircevski.com>
2018-09-23 12:14:11 -0400

    Switch to CNNs in sample-model3.
    Signed-off-by: Dejan Mircevski <github@mircevski.com>

--- ai/sample-model3.py
+++ ai/sample-model3.py
@@ -13,7 +13,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""A sample Keras model trainable on a set of fuzzlogs.
+"""A sample Keras model trainable on a set of fuzzlogs.  The model is adapted
+from Alexander Rakhlin's sample implementation of NLP CNN:
+https://github.com/alexander-rakhlin/CNN-for-Sentence-Classification-in-Keras.
 
 Usage: $0 [epochs] [batch_size]
 Defaults: epochs=1, batch_size=50
@@ -27,10 +29,10 @@ train/$((counter++)).$?`.)
 """
 
 from keras.constraints import min_max_norm
-from keras.layers import BatchNormalization, Dense, Dropout
-from keras.layers import Embedding, Flatten, Input
-from keras.layers.merge import concatenate, multiply
-from keras.metrics import mse
+from keras.layers import BatchNormalization, Dense, Dropout, Conv1D
+from keras.layers import Embedding, Flatten, Input, MaxPooling1D
+from keras.layers.merge import concatenate
+from keras.metrics import binary_crossentropy
 from keras.models import Model
 from keras.optimizers import Adam
 import glob
@@ -75,9 +77,10 @@ def get_training_data(tree_root, poscount, locidx):
 locs, vals, labels = get_training_data(exetree, poscount, locidx)
 
 embedding_dim = 4
-dropout_prob = 0.4
-dense_count = int(sys.argv[3]) if len(sys.argv) > 3 else 50
-optr = Adam(lr=0.01)
+filter_sizes = (3, 8)
+num_filters = 1
+dropout_prob = (0.01, 0.01)
+hidden_dims = 10
 
 in_vals = Input((poscount, 1), name='vals', dtype='float64')
 normd = BatchNormalization(
@@ -87,14 +90,17 @@ in_locs = Input((poscount, ), name='locs', dtype='uint64')
 embed_locs = Embedding(
     locidx.watermark, embedding_dim, input_length=poscount)(in_locs)
 merged = concatenate([embed_locs, normd])
-dense_list = []
-for i in range(dense_count):
-    dense_list.append(
-        Dropout(dropout_prob)(Dense(1,
-                                    activation='sigmoid')(Flatten()(merged))))
-mult = multiply(dense_list)
-ml = Model(inputs=[in_locs, in_vals], outputs=mult)
-ml.compile(optr, metrics=['acc'], loss=mse)
+drop = Dropout(dropout_prob[0])(merged)
+conv_list = []
+for filtsz in filter_sizes:
+    tmp = Conv1D(num_filters, filtsz, activation='relu')(drop)
+    tmp = Flatten()(MaxPooling1D()(tmp))
+    conv_list.append(tmp)
+out = Dense(
+    1, activation='sigmoid')(Dense(hidden_dims, activation='relu')(Dropout(
+        dropout_prob[1])(concatenate(conv_list))))
+ml = Model(inputs=[in_locs, in_vals], outputs=out)
+ml.compile(Adam(lr=0.01), metrics=['acc'], loss=binary_crossentropy)
 
 
 def fit(eps, bsz):
